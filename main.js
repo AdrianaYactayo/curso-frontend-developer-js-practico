5558882971d3f6f7ea6a9dd7756e5191eeb1206d
ADRIANA YACTAYO SULLER <adrianayactayosuller@gmail.com>
2023-01-26 07:07:35 -0500

    carrito

--- main.js
+++ main.js
@@ -5,14 +5,40 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 function toggleDesktopMenu(){
-   
+    const isAsideClose=aside.classList.contains('inactive');
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-   
+    const isAsideClose=aside.classList.contains('inactive');
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
+   
 }
+function toggleCarritoAside(){
+   
+    const ismobileMenuClose=mobileMenu.classList.contains('inactive');
+   
+    if(!ismobileMenuClose){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive');
+   
+
+    
+}
+
